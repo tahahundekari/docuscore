3a7e5983877b09abe1eeecd2c363fc07e7b9f6c8
Taha Hundekari <ge47mut@mytum.de>
2023-11-18 19:09:11 +0100

    add scoreboard logic (not functional)

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 import * as vscode from "vscode";
 import { runMessageParser } from "./message-parsing/message-parser";
 import { createFoldingIcon } from "./messageFolding/messageFolding";
+import { scoreboard } from "./scoreboard/plot-scoreboard";
 
 // this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
   runMessageParser(context);
   createFoldingIcon(context);
+  scoreboard();
 }
 
 export function deactivate() {}
