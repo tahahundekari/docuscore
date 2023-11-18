3a040c7796dbcb8073351c84be27b505bacdbf5a
Anton Scheitler <anton.scheitler@gmail.com>
2023-11-18 17:28:42 +0100

    upvote-button skeleton added

---
+++ src/upvote-button/upvote-button.ts
@@ -0,0 +1,7 @@
+import * as vscode from "vscode";
+
+export function createUpvoteButton(context: vscode.ExtensionContext) {
+  createIconAtLine(10);
+}
+
+function createIconAtLine(lineNumber: number) {}
