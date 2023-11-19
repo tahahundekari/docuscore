e3f67f399243cf9d00f1433ef0499f1b756e4c39
Anton Scheitler <anton.scheitler@gmail.com>
2023-11-19 04:01:15 +0100

    author logging added

--- src/upvote-comment/upvote-comment.ts
+++ src/upvote-comment/upvote-comment.ts
@@ -1,11 +1,20 @@
 import * as vscode from "vscode";
+const { blameLine } = require("git-blame-line");
 
 export function upvoteComment() {}
+//a! this will be used later
 export function registerUpvoteCommand(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "DocUScore.upvote",
     (fileLine: string) => {
-      vscode.window.showInformationMessage(`received message: ${fileLine}`);
+      blameLine(fileLine)
+        .then((info: any) => {
+          vscode.window.showInformationMessage(`author: ${info}`);
+          console.log(info);
+        })
+        .catch((error: any) => {
+          vscode.window.showErrorMessage("Can only upvote in a git repo");
+        });
     }
   );
 
